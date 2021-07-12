afdc9518225ec8917c51a22a1324f6f03640d84d
Andy Castillo <andy.castillopalma@outlook.com>
2021-07-12 14:40:03 -0500

    first commit

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if($request->ajax())
+    	{
+    		$data = Event::whereDate('start', '>=', $request->start)->whereDate('end', '<=', $request->end)->get(['id', 'title', 'start', 'end']);
+            return response()->json($data);
+    	}
+    	return view('fullcalendar');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function action(Request $request)
+    {
+    	if($request->ajax())
+    	{
+    		if($request->type == 'add')
+    		{
+    			$event = Event::create([
+    				'title'		=>	$request->title,
+    				'start'		=>	$request->start,
+    				'end'		=>	$request->end
+    			]);
+
+    			return response()->json($event);
+    		}
+
+    		if($request->type == 'update')
+    		{
+    			$event = Event::find($request->id)->update([
+    				'title'		=>	$request->title,
+    				'start'		=>	$request->start,
+    				'end'		=>	$request->end
+    			]);
+
+    			return response()->json($event);
+    		}
+
+    		if($request->type == 'delete')
+    		{
+    			$event = Event::find($request->id)->delete();
+    			return response()->json($event);
+    		}
+    	}
+    }
+}
